d91f199aa001ffd88e940f82aced9ad88924900b
Christian Gast <Christian.Gast@gmail.com>
2015-02-17 12:42:42 +0100

    Update server.py

--- server.py
+++ server.py
@@ -5,22 +5,23 @@ import json
 from urlparse import urlparse, parse_qs
 
 class MyRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
-    def do_GET(self):
+
+	def sendPageId(self):
+		global pageId
+		self.send_response(200)
+		self.send_header('Content-Type', 'application/json')
+		self.end_headers()
+		self.wfile.write(json.dumps(pageId))
+	
+	def do_GET(self):
 		global pageId
 		if self.path.startswith('/setPage/'):
 			pageId = self.path[9:]
 			print 'setting page to ' + pageId
-			self.send_response(200)
-			self.send_header('Content-Type', 'application/json')
-			self.end_headers()
-			self.wfile.write(json.dumps(pageId))
+			self.sendPageId()
 			return
 		elif self.path.startswith('/getPage'):
-			print 'current page is ' + pageId
-			self.send_response(200)
-			self.send_header('Content-Type', 'application/json')
-			self.end_headers()
-			self.wfile.write(json.dumps(pageId))
+			self.sendPageId()
 			return
 		else:
 			return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
